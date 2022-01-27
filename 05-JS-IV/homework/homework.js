0124a72ef14099b11fdcbc430b67872856a297e1
Jonathandanielperez <jonathantj63@gmail.com>
2022-01-26 23:09:00 -0300

    cuarto2 commit

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -155,8 +155,8 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-producto.calcularPrecioDescuento= function(){
-  (producto.precio * porcentajeDeDescuento) - producto.precio;
+producto.calcularPrecioDescuento = function(){
+  return this.precio - (this.precio * this.porcentajeDeDescuento);
 };
   return producto;
 }
